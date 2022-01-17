9bda2cdbe8993b71f6837ffab36f3748c7f7ad98
Alex D <alex.darincev@gmail.com>
2022-01-17 14:41:38 +0200

    feat(elements): int cards addition using javaScript enabled

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,74 @@ let profileEditBtn = profile.querySelector(".profile-info__button-edit");
 let popup = document.querySelector(".popup");
 let popupCloseBtn = popup.querySelector(".popup__button-close");
 
+const cardContainer = document.querySelector(".elements");
+const cardTemplate = document.querySelector("#template-element").content;
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+const someCard = {
+  name: "Yosemite Valley",
+  link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+};
+
+generateInitialCards(initialCards);
+generateCard(someCard);
+
+function generateInitialCards(cardDataArray) {
+  for (let index = 0; index < cardDataArray.length; index++) {
+    generateCard(cardDataArray[index]);
+  }
+}
+
+function createCard(item) {
+  const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".element__image");
+  const cardTitle = cardElement.querySelector(".element__title");
+  const cardButtonLike = cardElement.querySelector(".element__button-like");
+
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+
+  cardTitle.textContent = item.name;
+
+  cardButtonLike.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("element__button-like_active");
+  });
+
+  return cardElement;
+}
+
+function generateCard(cardData) {
+  const placeCard = createCard(cardData);
+  cardContainer.append(placeCard);
+}
+
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
 
