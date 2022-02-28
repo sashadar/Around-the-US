bf8022fcfd14f7ea7b4f2080db4be3cb1237dd4c
Alex D <alex.darincev@gmail.com>
2022-02-28 09:24:43 +0200

    feat: moved a constants from index.js to constants.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,7 @@
 import Card from "./Card.js";
 import FormValidator from "./FormValidator.js";
 import * as utils from "./utils.js";
-
-const validationSettings = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
-};
-
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
-  },
-];
+import { validationSettings, initialCards } from "./constants.js";
 
 const cardContainer = document.querySelector(".elements");
 const formEditProfile = document.getElementsByName("form-edit-profile")[0];
