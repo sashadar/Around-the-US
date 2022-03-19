9fa6d48b2a79e8ac2d9991555988544705bfd601
Alex D <alex.darincev@gmail.com>
2022-03-19 10:58:14 +0200

    refact: UserInfo.js created

---
+++ scripts/UserInfo.js
@@ -0,0 +1,21 @@
+class UserInfo {
+  constructor({ nameSelector, jobSelector }) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._name = this._nameElement.textContent;
+    this._jobElement = document.querySelector(jobSelector);
+    this._job = this._jobElement.textContent;
+  }
+
+  getUserInfo() {
+    this._userInfo = { name: this._name, job: this._job };
+
+    return this._userInfo;
+  }
+
+  setUserInfo(name, job) {
+    this._nameElement.textContent = name;
+    this._jobElement.textContent = job;
+  }
+}
+
+export default UserInfo;
