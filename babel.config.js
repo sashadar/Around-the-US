7ca9b63f2beaf4194a0b9dac1fc0ef7af609c7a0
Alex D <alex.darincev@gmail.com>
2022-03-25 18:52:46 +0300

    refact: setting updated

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,13 @@
 const presets = [
   [
-    "@babel/env",
+    "@babel/preset-env",
     {
-      targets: {
-        edge: "17",
-        ie: "11",
-        firefox: "50",
-        chrome: "64",
-        safari: "11.1",
-      },
+      // preset you want to use
+      // browser versions in which we want our code supported
+      targets: "defaults, not IE 11, not dead",
 
+      // use polyfills for the browsers specified in the above targets option
+      // Babel uses polyfills from the core-js library
       useBuiltIns: "entry",
       corejs: "^3",
     },
